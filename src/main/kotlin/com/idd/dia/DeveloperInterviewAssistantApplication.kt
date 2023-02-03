4be636b742622a977d38c503a13636fa0b93f63d
jay <jaeykweon@naver.com>
2023-02-03 23:49:20 +0900

    init

---
+++ src/main/kotlin/com/idd/dia/DeveloperInterviewAssistantApplication.kt
@@ -0,0 +1,11 @@
+package com.idd.dia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeveloperInterviewAssistantApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeveloperInterviewAssistantApplication>(*args)
+}
---
+++ src/test/kotlin/com/idd/dia/DeveloperInterviewAssistantApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.idd.dia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeveloperInterviewAssistantApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
