24c7531750f66eb08f8ace0a1ed60ece94e0c51a
jay <jaeykweon@naver.com>
2023-03-16 02:00:28 +0900

    v0.5.1 oauth controller -> restcontroller

--- src/main/kotlin/com/idd/dia/infra/web/AuthController.kt
+++ src/main/kotlin/com/idd/dia/infra/web/AuthController.kt
@@ -4,13 +4,13 @@ import com.idd.dia.application.dto.TokenResponse
 import com.idd.dia.application.service.GithubLoginService
 import com.idd.dia.infra.security.JwtTokenProvider
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.constraints.NotEmpty
 
-@Controller
+@RestController
 @RequestMapping("/api/v0/auth")
 class AuthController(
     private val jwtTokenProvider: JwtTokenProvider,
